efa85d3a854ac068fef89ba7425c47c9b46f439a
boboJavaa <bobo.mailes@gmail.com>
2023-04-08 16:54:25 +0200

    config

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const container = document.querySelector('#container');
 
+document.querySelector('#btn').addEventListener('click', function() {
+    let userInput = prompt("Enter size of grid , e.g: 64 = 64x64");
+    if(userInput <= 100){
+    removeGrid();   
+    createGrid(userInput);
+    isSelected(); 
+}
+})
+
 function createGrid(x) {
     for (let rows = 0; rows < x; rows++) {
         for (let columns = 0; columns < x; columns++) {
@@ -15,9 +24,11 @@ function createGrid(x) {
         gridElement.style.height = gridSize + 'px';
     });
 }
-
-
-createGrid(16);
+function removeGrid() {
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+}
 
 function isSelected() {
     const gridElements = document.querySelectorAll('.grid');
@@ -26,11 +37,10 @@ function isSelected() {
             if (gridElement.matches(':hover')) {
                 let r = Math.floor(Math.random() * 256) 
                 let g = Math.floor(Math.random() * 256)
-                let b = Math.floor(Math.random() * 256)
+                let b = Math.floor(Math.random() * 256)             
                 gridElement.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")"; 
             }
         });
     });
 }
 
-isSelected();
